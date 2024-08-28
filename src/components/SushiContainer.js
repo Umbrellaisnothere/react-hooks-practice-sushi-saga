d2910d6ec960f83e3df6d8bad10269e7c548c152
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-08-29 01:06:53 +0300

    Completed the  practice challenge, Created a Wallet component

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Sushi from "./Sushi";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, onEatSushi }) {
+
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+  const sushiComponents = sushis
+    .slice(sushiIndex, sushiIndex + 4)
+    .map((sushi) => (
+      <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi} />
+    ));
+
+  function handleClickMore() {
+  
+    setSushiIndex((sushiIndex) => (sushiIndex + 4) % sushis.length);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      
+      <MoreButton onClickMore={handleClickMore} />
     </div>
   );
 }
